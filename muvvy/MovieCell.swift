80767cce7d606d32ef0db22cd605b49af6aa82d8
Harley Trung <harley@harley.io>
2015-08-27 16:11:38 +0700

    set up movie cell with title and synopsis

---
+++ muvvy/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  muvvy
+//
+//  Created by Harley Trung on 8/27/15.
+//  Copyright (c) 2015 Harley Trung. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
